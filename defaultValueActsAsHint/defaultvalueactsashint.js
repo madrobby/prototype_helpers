bc651bd6f30303d97f939909a63fc196f2f37978
Thomas Fuchs <thomas@fesch.at>
2009-01-07 14:06:52 +0100

    add a test for textareas

--- defaultValueActsAsHint/defaultvalueactsashint.js
+++ defaultValueActsAsHint/defaultvalueactsashint.js
@@ -1,16 +1,18 @@
-Element.addMethods('input', {
-  
-  defaultValueActsAsHint: function(element){
-    element = $(element);
-    element._default = element.value;
-    
-    return element.observe('focus', function(){
-      if(element._default != element.value) return;
-      element.removeClassName('hint').value = '';
-    }).observe('blur', function(){
-      if(element.value.strip() != '') return;
-      element.addClassName('hint').value = element._default;
-    }).addClassName('hint');
-  }
-  
-});
+(function(){
+  var methods = {
+    defaultValueActsAsHint: function(element){
+      element = $(element);
+      element._default = element.value;
+      
+      return element.observe('focus', function(){
+        if(element._default != element.value) return;
+        element.removeClassName('hint').value = '';
+      }).observe('blur', function(){
+        if(element.value.strip() != '') return;
+        element.addClassName('hint').value = element._default;
+      }).addClassName('hint');
+    }
+  };
+   
+  $w('input textarea').each(function(tag){ Element.addMethods(tag, methods) });
+})();
